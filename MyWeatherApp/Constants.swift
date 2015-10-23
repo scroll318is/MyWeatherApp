5422de8a9b04e5711309b3a5aeef0a9663169491
Stoyan Kostov <stoyanykostov@gmail.com>
2015-10-23 15:01:35 +0300

    Initial commit

---
+++ MyWeatherApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MyWeatherApp
+//
+//  Created by Stoyan Yordanov Kostov on 10/23/15.
+//  Copyright © 2015 com.kostov. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "&appid=2f36fec44dc4baf2c53aeb1d2c8bad8e"
+let URL_BASE = "http://api.openweathermap.org/data/2.5/weather?q=Sofia"
---
+++ MyWeatherApp/DownloadManager.swift
@@ -0,0 +1,9 @@
+//
+//  DownloadManager.swift
+//  MyWeatherApp
+//
+//  Created by Stoyan Yordanov Kostov on 10/23/15.
+//  Copyright © 2015 com.kostov. All rights reserved.
+//
+
+import Foundation
